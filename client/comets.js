3d350307fa8ee95c720b2f905fb1b1a4a9266ade
William Ledoux <william.ledoux@gmail.com>
2014-10-19 12:57:24 +0200

    Renderer works with its own set of objects instead of fetching database every frame, so that it can add persistent properties to objects

--- client/comets.js
+++ client/comets.js
@@ -1,11 +1,12 @@
 //-----------------------------------------------------------------------------
 // CONFIG
 var config={
-  'framerate'   : 1/60,
-  'originX'     : 400,
-  'originY'     : 400,
-  'minRadius'   : 150,
-  'maxRadius'   : 380,
+  'framerate'         : 1000/60, // 60 FPS
+  'originX'           : 400,
+  'originY'           : 400,
+  'minRadius'         : 150,
+  'maxRadius'         : 380,
+  'appearanceDuration': 1000, 
 };
 //-----------------------------------------------------------------------------
 // STARTUP
@@ -31,57 +32,134 @@ Meteor.startup(function(){
     'right_tail_length'    : 30,
     'right_tail_color'     : "#FFFF00"
   });
-
-  Session.set("time", 0);
-  Meteor.setInterval(function(){
-    Session.set("time", (Session.get("time")+1)%360 );
-  }, config.framerate);
-
 });
 //-----------------------------------------------------------------------------
-// D3 BINDINGS
-var gTime;
-function bindCenterX(d, i){ return (config.originX + d.rotation_radius * Math.cos((gTime+d.rotation_start_angle)*d.speed*Math.PI/180)); }
-function bindCenterY(d, i){ return (config.originY + d.rotation_radius * Math.sin((gTime+d.rotation_start_angle)*d.speed*Math.PI/180)); }
-function bindCenterRadius(d){ return d.center_size; }
-function bindCenterColor(d){ return d.center_color; }
-function bindStrokeSize(d){ return d.stroke_size; }
-function bindStrokeColor(d){ return d.stroke_color; }
+// D3 COMETS RENDERING
+var CometsRenderer = function(selector, config){
+  var self=this;
+
+  self.selector = selector;
+  self.comets=[];
+  self.time = 0;
+  self.framerate          = config.framerate          || 1000/60; // 60 FPS
+  self.originX            = config.originX            || 400;
+  self.originY            = config.originY            || 400;
+  self.minRadius          = config.minRadius          || 150;
+  self.maxRadius          = config.maxRadius          || 380;
+  self.appearanceDuration = config.appearanceDuration || 1000;
+
+  self.bindCenterX = function(d, i){
+    return (self.originX + d.rotation_radius * Math.cos((self.time+d.rotation_start_angle)*d.speed*Math.PI/180)); 
+  };
+  self.bindCenterY = function(d, i){
+    return (self.originY + d.rotation_radius * Math.sin((self.time+d.rotation_start_angle)*d.speed*Math.PI/180)); 
+  };
+  self.bindFutureCenterX = function(d, i){
+    return (self.originX + d.rotation_radius * Math.cos((self.futureTime+d.rotation_start_angle)*d.speed*Math.PI/180)); 
+  };
+  self.bindFutureCenterY = function(d, i){
+    return (self.originY + d.rotation_radius * Math.sin((self.futureTime+d.rotation_start_angle)*d.speed*Math.PI/180)); 
+  };
+  self.bindCenterRadius = function(d){
+    return d.center_size; 
+  };
+  self.bindCenterColor = function(d){
+    return d.center_color; 
+  };
+  self.bindStrokeSize = function(d){
+    return d.stroke_size; 
+  };
+  self.bindStrokeColor = function(d){
+    return d.stroke_color; 
+  };
+
+  Meteor.setInterval(function(){
+    self.render();
+  }, self.framerate);
+};
 //-----------------------------------------------------------------------------
-// D3 RENDERING
-function render(){
-  
-  gTime = Session.get("time");
-  var elements = Comets.find().fetch();
-  var svg = d3.select("svg#comets");
+CometsRenderer.prototype = {
+  //-----------------------------------------------------------------------------
+  addComet : function(comet){
+    this.comets.push(comet);
+  },
+  //-----------------------------------------------------------------------------
+  removeComet : function(comet){
+    this.comets = _.reject(this.comets, function(elt){ return elt._id == comet._id; });
+  },
+  //-----------------------------------------------------------------------------
+  changeComet : function(comet){
+    var Oldcomet = _.find(this.comets, function(elt){ return elt._id == comet._id; });
+    for(var attr in comet ){
+      if (comet.hasOwnProperty(attr)){
+        Oldcomet[attr] = comet[attr];
+      }
+    }
+  },
+  //-----------------------------------------------------------------------------
+  render : function(){
+    var self=this;
 
-  var circles = svg.selectAll("circle").data(elements, function(d){return d._id;});
+    self.time = self.time+1;
+    self.futureTime = self.time + self.appearanceDuration/self.framerate;
 
-  circles.enter().append("circle").attr({
-              'cx'           : bindCenterX,
-              'cy'           : bindCenterY,
-              'r'            : bindCenterRadius,
-              'fill'         : bindCenterColor,
-              'stroke'       : bindStrokeColor,
-              'stroke-width' : bindStrokeSize,
+    var svg = d3.select(self.selector);
+    var circles = svg.selectAll("circle").data(self.comets, function(d){return d._id;});
+
+    circles.enter()
+           .append("circle")
+           .each(function(d,i){d.lifetime=-1;})
+           .attr({
+              'cx'           : self.originX,
+              'cy'           : self.originY,
+              'r'            : 2,
+              'fill'         : "#ffffff",
+              'stroke'       : 1,
+              'stroke-width' : "#ffffff",
+            })
+           .transition()
+           .each("end", function(d,i){d.lifetime=0;})
+           .duration(self.appearanceDuration)
+           .attr({
+              'cx'           : self.bindFutureCenterX,
+              'cy'           : self.bindFutureCenterY,
+              'r'            : self.bindCenterRadius,
+              'fill'         : self.bindCenterColor,
+              'stroke'       : self.bindStrokeColor,
+              'stroke-width' : self.bindStrokeSize,
             });
 
-  circles.attr({
-              'cx'           : bindCenterX,
-              'cy'           : bindCenterY,
-              'r'            : bindCenterRadius,
-              'fill'         : bindCenterColor,
-              'stroke'       : bindStrokeColor,
-              'stroke-width' : bindStrokeSize,
-            });
+    circles.filter(function(d) { return d.lifetime>=0; })
+           .attr({
+                'cx'           : self.bindCenterX,
+                'cy'           : self.bindCenterY,
+                'r'            : self.bindCenterRadius,
+                'fill'         : self.bindCenterColor,
+                'stroke'       : self.bindStrokeColor,
+                'stroke-width' : self.bindStrokeSize,
+              });
 
-  circles.exit()
-         .style("opacity","0")
-         .remove();
-  
-}
+    circles.exit()
+           .style("opacity","0")
+           .remove();
+  }
+  //-----------------------------------------------------------------------------
+};
 //-----------------------------------------------------------------------------
 Template.comets.rendered = function(){
-  this.autorun(render);
+  var renderer = new CometsRenderer("svg#comets", config);
+
+  Comets.find().observe({
+    added: function (doc) {
+      renderer.addComet(doc);
+    },
+    removed: function (doc) {
+      renderer.removeComet(doc);
+    },
+    changed: function(doc){
+      renderer.changeComet(doc);
+    }
+  });
+
 };
 //-----------------------------------------------------------------------------
