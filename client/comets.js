c92412d4bbc937274fe5ffb34359cfe1940546dd
William Ledoux <william.ledoux@gmail.com>
2014-10-19 18:14:17 +0200

    added tails rendering

--- client/comets.js
+++ client/comets.js
@@ -18,19 +18,89 @@ Meteor.startup(function(){
     'rotation_radius'      : 200,
     'rotation_start_angle' : 0, // in degrees
     'rotation_clockwise'   : true,
-    'center_size'          : 5,
-    'center_color'         : "#231F20",
+    'center_size'          : 11,
+    'center_color'         : "#FFFFFF",
     'stroke_size'          : 1,
-    'stroke_color'         : "#FFFFFF",
-    'center_tail_width'    : 3,
-    'center_tail_length'   : 20,
-    'center_tail_color'    : "#FFAA00",
-    'left_tail_width'      : 2,
-    'left_tail_length'     : 10,
-    'left_tail_color'      : "#FF0000",
-    'right_tail_width'     : 2,
-    'right_tail_length'    : 30,
-    'right_tail_color'     : "#FFFF00"
+    'stroke_color'         : "#231F20",
+    'tails'                : [
+      {
+        'offset' : -10,
+        'width'  : 1,
+        'length' : 5,
+        'color'  : "#FFFFFF",
+        'opacity': 0.1
+      },
+      {
+        'offset' : -8,
+        'width'  : 1,
+        'length' : 15,
+        'color'  : "#FFFFFF",
+        'opacity': 0.2
+      },
+      {
+        'offset' : -6,
+        'width'  : 1,
+        'length' : 30,
+        'color'  : "#FFFFFF",
+        'opacity': 0.3
+      },
+      {
+        'offset' : -4,
+        'width'  : 1,
+        'length' : 45,
+        'color'  : "#FFFFFF",
+        'opacity': 0.4
+      },
+      {
+        'offset' : -2,
+        'width'  : 1,
+        'length' : 60,
+        'color'  : "#FFFFFF",
+        'opacity': 0.5
+      },
+      {
+        'offset' : 0,
+        'width'  : 1,
+        'length' : 75,
+        'color'  : "#FFFFFF",
+        'opacity': 0.6
+      },
+      {
+        'offset' : 2,
+        'width'  : 1,
+        'length' : 60,
+        'color'  : "#FFFFFF",
+        'opacity': 0.5
+      },
+      {
+        'offset' : 4,
+        'width'  : 1,
+        'length' : 45,
+        'color'  : "#FFFFFF",
+        'opacity': 0.4
+      },
+      {
+        'offset' : 6,
+        'width'  : 1,
+        'length' : 30,
+        'color'  : "#FFFFFF",
+        'opacity': 0.3
+      },
+      {
+        'offset' : 8,
+        'width'  : 1,
+        'length' : 15,
+        'color'  : "#FFFFFF",
+        'opacity': 0.2
+      },
+      {
+        'offset' : 10,
+        'width'  : 1,
+        'length' : 5,
+        'color'  : "#FFFFFF",
+        'opacity': 0.1
+      }
+    ]
   });
 });
 //-----------------------------------------------------------------------------
@@ -38,6 +108,7 @@ Meteor.startup(function(){
 var CometsRenderer = function(selector, config){
   var self=this;
 
+  self.τ = 2 * Math.PI; // http://tauday.com/tau-manifesto
   self.selector = selector;
   self.comets=[];
   self.time = 0;
@@ -47,18 +118,36 @@ var CometsRenderer = function(selector, config){
   self.minRadius          = config.minRadius          || 150;
   self.maxRadius          = config.maxRadius          || 380;
   self.appearanceDuration = config.appearanceDuration || 1000;
+  self.arc = d3.svg.arc()
+    .innerRadius(function(d){ 
+      return d.rotation_radius + d.offset;
+    })
+    .outerRadius(function(d){ 
+      return d.rotation_radius + d.offset + d.width;
+    })
+    .startAngle(function(d){ 
+      var startAngle = self.τ/4 + (self.time*d.speed +d.rotation_start_angle)*self.τ/360;
+      //console.log(startAngle);
+      return startAngle;
+    })
+    .endAngle(function(d){ 
+      var startAngle = self.τ/4 + (self.time*d.speed +d.rotation_start_angle)*self.τ/360;
+      var r = d.rotation_radius + d.offset + d.width/2;
+      var deltaAngle = d.length / r;
+      return startAngle - deltaAngle;
+    });
 
   self.bindCenterX = function(d, i){
-    return (self.originX + d.rotation_radius * Math.cos((self.time+d.rotation_start_angle)*d.speed*Math.PI/180)); 
+    return (d.rotation_radius * Math.cos((self.time+d.rotation_start_angle)*d.speed*self.τ/360)); 
   };
   self.bindCenterY = function(d, i){
-    return (self.originY + d.rotation_radius * Math.sin((self.time+d.rotation_start_angle)*d.speed*Math.PI/180)); 
+    return (d.rotation_radius * Math.sin((self.time+d.rotation_start_angle)*d.speed*self.τ/360)); 
   };
   self.bindFutureCenterX = function(d, i){
-    return (self.originX + d.rotation_radius * Math.cos((self.futureTime+d.rotation_start_angle)*d.speed*Math.PI/180)); 
+    return (d.rotation_radius * Math.cos((self.futureTime+d.rotation_start_angle)*d.speed*self.τ/360)); 
   };
   self.bindFutureCenterY = function(d, i){
-    return (self.originY + d.rotation_radius * Math.sin((self.futureTime+d.rotation_start_angle)*d.speed*Math.PI/180)); 
+    return (d.rotation_radius * Math.sin((self.futureTime+d.rotation_start_angle)*d.speed*self.τ/360)); 
   };
   self.bindCenterRadius = function(d){
     return d.center_size; 
@@ -80,7 +169,20 @@ var CometsRenderer = function(selector, config){
 //-----------------------------------------------------------------------------
 CometsRenderer.prototype = {
   //-----------------------------------------------------------------------------
+  checkComet : function(comet){
+    //TODO: validate comet properties
+
+    // Copy in each tail informations from the comet that are needed to render the tail
+    for(var iTail=0; iTail<comet.tails.length; ++iTail){
+      var tail = comet.tails[iTail];
+      tail.rotation_radius = comet.rotation_radius;
+      tail.rotation_start_angle = comet.rotation_start_angle;
+      tail.speed = comet.speed;
+    }
+  },
+  //-----------------------------------------------------------------------------
   addComet : function(comet){
+    this.checkComet(comet);
     this.comets.push(comet);
   },
   //-----------------------------------------------------------------------------
@@ -95,41 +197,51 @@ CometsRenderer.prototype = {
         Oldcomet[attr] = comet[attr];
       }
     }
+    this.checkComet(Oldcomet);
   },
   //-----------------------------------------------------------------------------
   render : function(){
     var self=this;
 
-    self.time = self.time+1;
+    self.time = (self.time+1)%360;
     self.futureTime = self.time + self.appearanceDuration/self.framerate;
 
     var svg = d3.select(self.selector);
-    var circles = svg.selectAll("circle").data(self.comets, function(d){return d._id;});
+    var groups = svg.selectAll("g").data(self.comets, function(d){return d._id;});
 
-    circles.enter()
-           .append("circle")
-           .each(function(d,i){d.lifetime=-1;})
-           .attr({
-              'cx'           : self.originX,
-              'cy'           : self.originY,
-              'r'            : 2,
-              'fill'         : "#ffffff",
-              'stroke'       : 1,
-              'stroke-width' : "#ffffff",
-            })
-           .transition()
-           .each("end", function(d,i){d.lifetime=0;})
-           .duration(self.appearanceDuration)
-           .attr({
-              'cx'           : self.bindFutureCenterX,
-              'cy'           : self.bindFutureCenterY,
-              'r'            : self.bindCenterRadius,
-              'fill'         : self.bindCenterColor,
-              'stroke'       : self.bindStrokeColor,
-              'stroke-width' : self.bindStrokeSize,
-            });
+    var groupsOnEnter=groups.enter()
+          .append("g")
+          .attr("transform", "translate(" + self.originX + "," + self.originY + ")");
+    
+    groupsOnEnter.selectAll("path").data(function(d){ return d.tails; }).enter()
+        .append("path")
+        .datum(function(d, i, j){return d;})
+        .style("opacity", 0)
+        .attr("d", self.arc);
 
-    circles.filter(function(d) { return d.lifetime>=0; })
+    groupsOnEnter.append("circle")
+             .each(function(d,i){d.lifetime=-1;})
+             .attr({
+                'cx'           : 0,
+                'cy'           : 0,
+                'r'            : 2,
+                'fill'         : "#ffffff",
+                'stroke'       : 1,
+                'stroke-width' : "#ffffff",
+              })
+             .transition()
+             .each("end", function(d,i){d.lifetime=0;})
+             .duration(self.appearanceDuration)
+             .attr({
+                'cx'           : self.bindFutureCenterX,
+                'cy'           : self.bindFutureCenterY,
+                'r'            : self.bindCenterRadius,
+                'fill'         : self.bindCenterColor,
+                'stroke'       : self.bindStrokeColor,
+                'stroke-width' : self.bindStrokeSize,
+              });
+   
+    groups.selectAll("circle").filter(function(d) { return d.lifetime>=0; })
            .attr({
                 'cx'           : self.bindCenterX,
                 'cy'           : self.bindCenterY,
@@ -139,7 +251,13 @@ CometsRenderer.prototype = {
                 'stroke-width' : self.bindStrokeSize,
               });
 
-    circles.exit()
+    groups.filter(function(d) { return d.lifetime>=0; }).selectAll("path").data(function(d){ return d.tails; })
+        .datum(function(d, i, j){return d;})
+        .style("fill", function(d){return d.color;})
+        .style("opacity", function(d){return d.opacity;})
+        .attr("d", self.arc);        
+
+    groups.exit()
            .style("opacity","0")
            .remove();
   }
